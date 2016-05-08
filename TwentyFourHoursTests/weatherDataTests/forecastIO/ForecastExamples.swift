ce8ce0ae813fc9c58a0f406416ed4f6b0aec2a3f
vjosullivan <vjosullivan@gmail.com>
2016-05-08 10:21:30 +0100

    WeatherLine parsing added.

---
+++ TwentyFourHoursTests/weatherDataTests/forecastIO/ForecastExamples.swift
@@ -0,0 +1,9 @@
+//
+//  ForecastExamples.swift
+//  TwentyFourHours
+//
+//  Created by Vincent O'Sullivan on 08/05/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
