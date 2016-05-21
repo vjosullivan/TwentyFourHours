e6bdcb66bf5bc6fba0ed83f765fcc2c93af05f95
vjosullivan <vjosullivan@gmail.com>
2016-05-21 16:59:38 +0100

    First 'display lines' added.

---
+++ TwentyFourHours/weatherData/DisplayableForecast.swift
@@ -0,0 +1,9 @@
+//
+//  DisplayableForecast.swift
+//  TwentyFourHours
+//
+//  Created by Vincent O'Sullivan on 21/05/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import Foundation
